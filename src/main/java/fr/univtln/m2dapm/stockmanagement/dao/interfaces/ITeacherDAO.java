84cd65afac7c2173c849d49243cc44800e110b25
Gajovski Maxime <gajovski.maxime@gmail.com>
2017-01-26 21:43:31 +0100

    Adds DAO Package

--- src/main/java/fr/univtln/m2dapm/stockmanagement/dao/interfaces/ITeacherDAO.java
+++
@@ -1,9 +0,0 @@
-package fr.univtln.m2dapm.stockmanagement.dao.interfaces;
-
-import fr.univtln.m2dapm.stockmanagement.entities.interfaces.actors.ITeacher;
-
-/**
- * Created by Maxime Gajovski on 17/12/2016.
- */
-public interface ITeacherDAO extends ICrud<Long, ITeacher> {
-}
